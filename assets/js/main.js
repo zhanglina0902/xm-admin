14b58a5eba1df60845763fdfa77d553fc301475b
zhanglina0902 <81438901@qq.com>
2018-05-30 19:45:28 +0800

    huamingce

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,24 @@
     // tabs
     $('#new-tabs').tabs({noSwipe: 1});
 
-    // 
+    // paginations
+    $('.paginations').pagination({
+        pageCount: 50,
+        jump: true,
+        coping: true,
+        homePage: '首页',
+        endPage: '末页',
+        prevContent: '上页',
+        nextContent: '下页',
+        callback: function (api) {
+            console.log(api.getCurrent())
+        }
+    });
+
+
+    // table
+
+    
   
    
   
